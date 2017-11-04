901c9ba776c89c4a5a8bd27800bd920f4f2724db
vanyadeg <id1997@yandex.ru>
2017-11-04 20:08:14 +0300

    draw distribution for regression task

--- emotion_intensity_reg.py
+++ emotion_intensity_reg.py
@@ -1,6 +1,22 @@
 import argparse
 
 from tweet_parser import get_tweets
+from scipy.stats import norm
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import ntpath
+
+
+def draw_distribution(name):
+    emotion_name = ntpath.basename(name).split('_')[1]
+    results = [t.res for t in tweets]
+    (mu, sigma) = norm.fit(results)
+    n, bins, patches = plt.hist(results, 70, normed=1, facecolor='green', alpha=0.75)
+    y = mlab.normpdf(bins, mu, sigma)
+    plt.plot(bins, y, 'r--', linewidth=2)
+    plt.grid(True)
+    plt.savefig('distribution_%s.png' % emotion_name)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='File name')
@@ -8,5 +24,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     tweets = get_tweets(args.data.read(), res_acc=5)
-    for tweet in tweets:
-        print tweet
+    draw_distribution(args.data.name)
